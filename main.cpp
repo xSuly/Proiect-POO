f42e1f5351ed44a07fed0b22d876752643925d88
xSuly <albeiliviu@gmail.com>
2021-10-31 16:17:47 +0200

    Tema 1 update

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
+
 //
 // Created by xSuly on 10/22/2021.
 //
 
+// Proiect POO - Aplicatie magazin ceasuri de sana
+// Albei Liviu-Andrei
+// 261
 
 #include <iostream>
 #include<string>
@@ -11,17 +15,21 @@ class Magazin{
     std::string nume_magazin;
     std::string oras;
     std::string adresa;
-    std::string program;
+    //std::string program;
+    int ora_deschidere;
+    int ora_inchidere;
     std::string numar_telefon;
     std::string site_web;
 public:
-    Magazin(const std::string &numeMagazin, const std::string &oras, const std::string &adresa, const std::string &program, const std::string &numarTelefon, const std::string &siteWeb) : nume_magazin(numeMagazin), oras(oras), adresa(adresa), program(program), numar_telefon(numarTelefon), site_web(siteWeb) {}
+    Magazin(const std::string &numeMagazin, const std::string &oras, const std::string &adresa, /*const std::string &program*/int ora_deschidere, int ora_inchidere, const std::string &numarTelefon, const std::string &siteWeb) : nume_magazin(numeMagazin), oras(oras), adresa(adresa), ora_inchidere(ora_inchidere), ora_deschidere(ora_deschidere), /*program(program)*/numar_telefon(numarTelefon), site_web(siteWeb) {}
 
     Magazin(const Magazin &copie){
         this->nume_magazin=copie.nume_magazin;
         this->oras=copie.oras;
         this->adresa=copie.adresa;
-        this->program=copie.program;
+        //this->program=copie.program;
+        this->ora_inchidere=copie.ora_inchidere;
+        this->ora_deschidere=copie.ora_deschidere;
         this->numar_telefon=copie.numar_telefon;
         this->site_web=copie.site_web;
     }
@@ -30,7 +38,9 @@ public:
         this->nume_magazin=copie.nume_magazin;
         this->oras=copie.oras;
         this->adresa=copie.adresa;
-        this->program=copie.program;
+        //this->program=copie.program;
+        this->ora_deschidere=copie.ora_deschidere;
+        this->ora_inchidere=copie.ora_inchidere;
         this->numar_telefon=copie.numar_telefon;
         this->site_web=copie.site_web;
         return *this;
@@ -41,7 +51,7 @@ public:
     }
 
     friend std::ostream &operator<<(std::ostream &os, const Magazin &magazin) {
-        os <<"Nume magazin: " <<magazin.nume_magazin<<std::endl<< "Oras: " << magazin.oras << std::endl << "Adresa: " << magazin.adresa << std::endl << "Orar: " << magazin.program<< std::endl << "Numar de telefon: " << magazin.numar_telefon << std::endl << "Adresa site web magazin: " << magazin.site_web << std::endl;
+        os <<"Nume magazin: " <<magazin.nume_magazin<<std::endl<< "Oras: " << magazin.oras << std::endl << "Adresa: " << magazin.adresa << std::endl << "Orar: " << magazin.ora_inchidere<<" "<<magazin.ora_deschidere<< std::endl << "Numar de telefon: " << magazin.numar_telefon << std::endl << "Adresa site web magazin: " << magazin.site_web << std::endl;
         return os;
     }
 
@@ -110,12 +120,26 @@ public:
 
 int main() {
 
-    Magazin m1("watchshop", "braila", "bulevardul decebal", "08:30 - 19:45", "0700111222", "watch.com");
-    Magazin m2("shopwatch", "galati", "bulevardul traian", "08:30 - 19:45", "0700333444", "shop.com");
-    Magazin m3("magazin ceasuri", "ramnicu sarat", "strada burebista", "08:30 - 19:45", "0700555666", "ceasuri.com");
-    Ceas ceasornicarie("rolex", "roly", 18500.0, 1997);
-    Client c1("george", "johnny", "buzau", "strada principatelor", {});
+    Magazin m1("Watchshop", "Braila", "Bulevardul Decebal", 8, 20, "0700111222", "watch.com");
+    Magazin m2("Shopwatch", "Galati", "Bulevardul Traian", 9, 19, "0700333444", "shop.com");
+    Magazin m3("Magazin Ceasuri", "Ramnicu Sarat", "Strada Burebista", 10, 20,  "0700555666", "ceasuri.com");
+    Ceas ceasornicarie("Rolex", "40MM ROLEX DAYTONA WATCH", 115000.0, 2021);
+    Client c1("George", "Johnny", "Buzau", "Strada Principatelor", {});
     c1.adaugare_produs(ceasornicarie);
+
+    Magazin m4("Magazin ceasuri de mana luxury", "Bucuresti", "Strada Unirii", 7, 16,  "0725364758", "luxurywatch.ro");
+    Ceas model2("Fossil", "Townsman ME3197 Black", 825.0, 2019);
+    Ceas model3("Emporio Armani", "Renato AR2448 Argintiu", 1010.0, 2015);
+
+    Client c2("Liviu", "Andrei", "Ramnicu Sarat", "Bulevardul 1 Decembrie 1918", {});
+    Client c3("Cosmin", "Costin", "Comuna Ciorasti", "Strada Scolii", {});
+    c2.adaugare_produs(model2);
+    c3.adaugare_produs(model3);
+
+    Client c4;
+    c4=c3;
+    c4.adaugare_produs(model3);
+
 
     return 0;
 }
